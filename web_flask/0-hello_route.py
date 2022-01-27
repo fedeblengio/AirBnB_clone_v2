6ca23b337e2a66ab3fda7700cbcccbae7c0c3d9b
Federico Blengio <fedeeblengio@gmail.com>
2022-01-27 16:37:14 +0000

    pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
 from flask import Flask
-
 app = Flask(__name__)
+
+"""Task-0"""
+
 
 @app.route('/', strict_slashes=False)
 def index():
-  return "Hello HBNB!" 
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.debug = True

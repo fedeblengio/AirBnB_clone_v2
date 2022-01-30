402f66649413e5939a8067184912e09146c397e5
Federico Blengio <fedeeblengio@gmail.com>
2022-01-30 01:19:14 +0000

    now yes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Task-5"""
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -44,4 +44,4 @@ def html(n):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5009)

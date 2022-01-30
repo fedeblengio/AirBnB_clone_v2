5de03736ecf50dd4a7a61b7a2932830dffbeca83
Federico Blengio <fedeeblengio@gmail.com>
2022-01-30 00:53:53 +0000

    fix tasks-2-3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,27 @@
+#!/usr/bin/python3
+"""Task-2"""
 from flask import Flask
 app = Flask(__name__)
 
-"""Task-2"""
-
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello_hbnb():
+    """hello"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """hbnb"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def replaceTexT(text):
+    """text"""
     text = text.replace("_", " ")
     return "C " + text
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host="0.0.0.0", port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,34 @@
+#!/usr/bin/python3
+"""Task-3"""
 from flask import Flask
 app = Flask(__name__)
 
-"""Task-3"""
-
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello_hbnb():
+    """hello"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """bnbn"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def replaceTexT(text):
+    """text"""
     text = text.replace("_", " ")
     return "C " + text
 
 
 @app.route('/python/<text>', strict_slashes=False)
 def replaceTexT_2(text):
+    """text2"""
     text = text.replace("_", " ")
     return "Python " + text
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host="0.0.0.0", port=5000)

94c0a5d2c37dcc49d84971fedd81dab9ec312b3b
Federico Blengio <fedeeblengio@gmail.com>
2022-01-30 00:48:02 +0000

    fix task-1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,7 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello_hbnb():
+    """hello"""
     return "Hello HBNB!"
 
 
@@ -15,5 +17,4 @@ def hbnb():
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host="0.0.0.0", port=5000)
